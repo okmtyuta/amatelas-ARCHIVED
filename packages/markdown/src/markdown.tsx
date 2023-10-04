52dec97421bc04f2648342112e0b09aa94cbfdfb
okmtyuta <okmtyuta@gmail.com>
2023-10-04 21:25:06 +0900

    auto-commit

--- packages/markdown/src/markdown.tsx
+++ packages/markdown/src/markdown.tsx
@@ -2,7 +2,14 @@ import ReactMarkdown from 'react-markdown'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 import remarkGfm from 'remark-gfm'
-import { components } from './components/components'
+import {
+  Blockquote,
+  Heading,
+  List,
+  ListItem,
+  Paragraph,
+  Typography
+} from '@okmtyuta/amatelas/server'
 
 export const Markdown = ({ children }: { children?: string }) => {
   if (!children) {
@@ -10,9 +17,83 @@ export const Markdown = ({ children }: { children?: string }) => {
   }
   return (
     <ReactMarkdown
-      remarkPlugins={[remarkGfm, remarkMath]}
+      remarkPlugins={[remarkMath, remarkGfm]}
       rehypePlugins={[rehypeKatex]}
-      components={components}
+      components={{
+        h1(props) {
+          return (
+            <Heading as="h1">
+              <Typography fontFamily="sans-serif-jp">
+                {props.children}
+              </Typography>
+            </Heading>
+          )
+        },
+
+        h2(props) {
+          return (
+            <Heading as="h2">
+              <Typography fontFamily="sans-serif-jp">
+                {props.children}
+              </Typography>
+            </Heading>
+          )
+        },
+        h3(props) {
+          return (
+            <Heading as="h3">
+              <Typography fontFamily="sans-serif-jp">
+                {props.children}
+              </Typography>
+            </Heading>
+          )
+        },
+        h4(props) {
+          return (
+            <Heading as="h4">
+              <Typography fontFamily="sans-serif-jp">
+                {props.children}
+              </Typography>
+            </Heading>
+          )
+        },
+        h5(props) {
+          return (
+            <Heading as="h5">
+              <Typography fontFamily="sans-serif-jp">
+                {props.children}
+              </Typography>
+            </Heading>
+          )
+        },
+        h6(props) {
+          return (
+            <Heading as="h6">
+              <Typography fontFamily="sans-serif-jp">
+                {props.children}
+              </Typography>
+            </Heading>
+          )
+        },
+        strong(props) {
+          return <Typography fontWeight="bold">{props.children}</Typography>
+        },
+        em(props) {
+          return <Typography fontStyle="italic">{props.children}</Typography>
+        },
+        p(props) {
+          return <Paragraph>{props.children}</Paragraph>
+        },
+        ul(props) {
+          return <List>{props.children}</List>
+        },
+        li(props) {
+          return <ListItem>{props.children}</ListItem>
+        },
+        blockquote(props) {
+          return <Blockquote>{props.children}</Blockquote>
+        }
+      }}
     >
       {children}
     </ReactMarkdown>
