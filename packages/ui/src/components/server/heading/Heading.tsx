5fb65aa72303a855be0b7a71fd7fd912d650afb2
okmtyuta <okmtyuta@gmail.com>
2023-10-12 21:12:19 +0900

    headingでmarginが適用されるように種適用されるように修正

--- packages/ui/src/components/server/heading/Heading.tsx
+++ packages/ui/src/components/server/heading/Heading.tsx
@@ -6,14 +6,25 @@ type HeadingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 
 export type HeadingProps<T extends ElementType> = {
   as?: HeadingType
+  noMargin?: boolean
 } & Omit<ComponentPropsWithoutRef<T>, 'tag'>
+
+const getMarginClass = (noMargin?: boolean) => {
+  if (noMargin) {
+    return styles['no-margin']
+  }
+
+  return styles['margin']
+}
 
 export const Heading = <T extends ElementType = 'h1'>({
   as,
   className,
+  noMargin,
   ...props
 }: HeadingProps<T>) => {
   const _H = as ?? 'h1'
+  const _marginClass = getMarginClass(noMargin)
 
-  return <_H {...props} className={(clsx(className), styles[_H])} />
+  return <_H {...props} className={clsx(className, styles[_H], _marginClass)} />
 }
