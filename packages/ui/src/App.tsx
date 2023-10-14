b5cc72ef594b8279eb8a727ea3d727c2eb2ef994
okmtyuta <okmtyuta@gmail.com>
2023-10-14 23:04:36 +0900

    auto-commit

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,9 +1,6 @@
-import { Heading, Frame, TextArea, List, ListItem } from './index'
+import { Frame, List, ListItem } from './index'
 import '@okmtyuta/awesome-css/reset.css'
 import './app.css'
-import { TextField } from './components/server/text-field/TextFiled'
-import { DiscSVG } from './svg'
-import { MarkerListItem } from './components/server/list'
 import { demoText } from './demo-text'
 
 export const App = () => {
