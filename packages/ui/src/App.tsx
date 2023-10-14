93a2f909eaf22731a9c2ecf2edb7abb797d06161
okmtyuta <okmtyuta@gmail.com>
2023-10-14 23:02:49 +0900

    auto-commit

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -2,37 +2,20 @@ import { Heading, Frame, TextArea, List, ListItem } from './index'
 import '@okmtyuta/awesome-css/reset.css'
 import './app.css'
 import { TextField } from './components/server/text-field/TextFiled'
+import { DiscSVG } from './svg'
+import { MarkerListItem } from './components/server/list'
+import { demoText } from './demo-text'
 
 export const App = () => {
   return (
     <>
       <Frame className="frame">
-        <Heading>Textfield</Heading>
-        <TextField validate variant="filled" placeholder="placeholder" />
-        <TextField validate variant="outlined" placeholder="placeholder" />
-        <TextField
-          validate
-          // pattern="\d{3}-\d{4}"
-          variant="standard"
-          placeholder="placeholder"
-          helper={
-            <List>
-              <ListItem>
-                郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。郵便番号を入力してください。
-              </ListItem>
-              <ListItem marker="done">郵便番号を入力してください。</ListItem>
-              <ListItem marker="dangerous">
-                郵便番号を入力してください。
-              </ListItem>
-            </List>
-          }
-          required
-        />
-
-        <Heading>Textarea</Heading>
-        <TextArea placeholder="outlined" variant="outlined" />
-        <TextArea placeholder="standard" variant="standard" />
-        <TextArea placeholder="filled" variant="filled" />
+        <List>
+          <ListItem>{demoText.en.long}</ListItem>
+          <ListItem marker="done">
+            基礎的な要素を表現するために使われます。
+          </ListItem>
+        </List>
       </Frame>
     </>
   )
