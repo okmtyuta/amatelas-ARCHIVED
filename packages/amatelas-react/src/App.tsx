55054dfb8bc3c316406b9663173a925ddff4051b
okmtyuta <okmtyuta@gmail.com>
2023-11-05 14:29:04 +0900

    auto-commit

--- packages/amatelas-react/src/App.tsx
+++ packages/amatelas-react/src/App.tsx
@@ -1,8 +1,6 @@
 import '@okmtyuta/awesome-css/reset.css'
 
-import { Checkbox, Frame, List, ListItem, Snack } from './components/server'
-import { Button } from './components/server/button/native/Button'
-import { MaterialChip } from './components/server/chip/material/MaterialChip'
+import { Frame, Snack } from './components/server'
 import { demoText } from './demo-text'
 
 export const App = () => {
